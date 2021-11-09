62a050d3e68a5883d23e77cb2859e6433e2f4a1b
sdjr2 <32803005+sdjr2@users.noreply.github.com>
2021-11-09 15:51:30 -0800

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 197157210925.dkr.ecr.us-west-2.amazonaws.com
-docker pull 197157210925.dkr.ecr.us-west-2.amazonaws.com/farnazdargahi/pywebapp:latest || {
+docker pull 197157210925.dkr.ecr.us-west-2.amazonaws.com/sdjr2/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
 }
-docker run --name pywebapp -p 80:8080 --detach 197157210925.dkr.ecr.us-west-2.amazonaws.com/farnazdargahi/pywebapp:latest
+docker run --name pywebapp -p 80:8080 --detach 197157210925.dkr.ecr.us-west-2.amazonaws.com/sdjr2/pywebapp:latest
